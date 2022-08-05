596e781a1aa640737641b555549935cf4b0eaefd
Alexandre <alex100dre.pro@gmail.com>
2022-08-05 14:25:41 +0200

    fix: allow CORS

--- server.ts
+++ server.ts
@@ -1,7 +1,15 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 import trashRoutes from "./src/routes/trash.routes";
+
 const app: Express = express();
 const port = process.env.PORT;
+// const allowedOrigins = [process.env.APP_URL] as string[];
+// const options: cors.CorsOptions = {
+//     origin: allowedOrigins
+// };
+
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
